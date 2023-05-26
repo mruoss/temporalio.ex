6f5065dbdb0da5e831bb750e9cc2e1188ed5b088
mruoss <mruoss@users.noreply.github.com>
2023-05-26 07:21:48 +0000

    [create-pull-request] automated change

--- lib/temporal/api/enums/v1/batch_operation.pb.ex
+++ lib/temporal/api/enums/v1/batch_operation.pb.ex
@@ -8,6 +8,7 @@ defmodule Temporal.Api.Enums.V1.BatchOperationType do
   field :BATCH_OPERATION_TYPE_CANCEL, 2
   field :BATCH_OPERATION_TYPE_SIGNAL, 3
   field :BATCH_OPERATION_TYPE_DELETE, 4
+  field :BATCH_OPERATION_TYPE_RESET, 5
 end
 
 defmodule Temporal.Api.Enums.V1.BatchOperationState do
