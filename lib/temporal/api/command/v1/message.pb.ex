f2a6435a573c7073b01f0494a8b277652451771c
mruoss <695307+mruoss@users.noreply.github.com>
2026-02-01 06:01:26 +0000

    [create-pull-request] automated change

--- lib/temporal/api/command/v1/message.pb.ex
+++ lib/temporal/api/command/v1/message.pb.ex
@@ -327,14 +327,6 @@ defmodule Temporal.Api.Command.V1.ScheduleNexusOperationCommandAttributes do
     type: Temporal.Api.Command.V1.ScheduleNexusOperationCommandAttributes.NexusHeaderEntry,
     json_name: "nexusHeader",
     map: true
-
-  field :schedule_to_start_timeout, 7,
-    type: Google.Protobuf.Duration,
-    json_name: "scheduleToStartTimeout"
-
-  field :start_to_close_timeout, 8,
-    type: Google.Protobuf.Duration,
-    json_name: "startToCloseTimeout"
 end
 
 defmodule Temporal.Api.Command.V1.RequestCancelNexusOperationCommandAttributes do
