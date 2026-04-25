d21d702f350f40bc42e27e6bd8fb7c9f887f155c
mruoss <695307+mruoss@users.noreply.github.com>
2026-04-25 06:21:59 +0000

    [create-pull-request] automated change

--- lib/temporal/api/enums/v1/nexus.pb.ex
+++ lib/temporal/api/enums/v1/nexus.pb.ex
@@ -11,3 +11,64 @@ defmodule Temporal.Api.Enums.V1.NexusHandlerErrorRetryBehavior do
   field :NEXUS_HANDLER_ERROR_RETRY_BEHAVIOR_RETRYABLE, 1
   field :NEXUS_HANDLER_ERROR_RETRY_BEHAVIOR_NON_RETRYABLE, 2
 end
+
+defmodule Temporal.Api.Enums.V1.NexusOperationExecutionStatus do
+  @moduledoc false
+
+  use Protobuf,
+    enum: true,
+    full_name: "temporal.api.enums.v1.NexusOperationExecutionStatus",
+    protoc_gen_elixir_version: "0.16.0",
+    syntax: :proto3
+
+  field :NEXUS_OPERATION_EXECUTION_STATUS_UNSPECIFIED, 0
+  field :NEXUS_OPERATION_EXECUTION_STATUS_RUNNING, 1
+  field :NEXUS_OPERATION_EXECUTION_STATUS_COMPLETED, 2
+  field :NEXUS_OPERATION_EXECUTION_STATUS_FAILED, 3
+  field :NEXUS_OPERATION_EXECUTION_STATUS_CANCELED, 4
+  field :NEXUS_OPERATION_EXECUTION_STATUS_TERMINATED, 5
+  field :NEXUS_OPERATION_EXECUTION_STATUS_TIMED_OUT, 6
+end
+
+defmodule Temporal.Api.Enums.V1.NexusOperationWaitStage do
+  @moduledoc false
+
+  use Protobuf,
+    enum: true,
+    full_name: "temporal.api.enums.v1.NexusOperationWaitStage",
+    protoc_gen_elixir_version: "0.16.0",
+    syntax: :proto3
+
+  field :NEXUS_OPERATION_WAIT_STAGE_UNSPECIFIED, 0
+  field :NEXUS_OPERATION_WAIT_STAGE_STARTED, 1
+  field :NEXUS_OPERATION_WAIT_STAGE_CLOSED, 2
+end
+
+defmodule Temporal.Api.Enums.V1.NexusOperationIdReusePolicy do
+  @moduledoc false
+
+  use Protobuf,
+    enum: true,
+    full_name: "temporal.api.enums.v1.NexusOperationIdReusePolicy",
+    protoc_gen_elixir_version: "0.16.0",
+    syntax: :proto3
+
+  field :NEXUS_OPERATION_ID_REUSE_POLICY_UNSPECIFIED, 0
+  field :NEXUS_OPERATION_ID_REUSE_POLICY_ALLOW_DUPLICATE, 1
+  field :NEXUS_OPERATION_ID_REUSE_POLICY_ALLOW_DUPLICATE_FAILED_ONLY, 2
+  field :NEXUS_OPERATION_ID_REUSE_POLICY_REJECT_DUPLICATE, 3
+end
+
+defmodule Temporal.Api.Enums.V1.NexusOperationIdConflictPolicy do
+  @moduledoc false
+
+  use Protobuf,
+    enum: true,
+    full_name: "temporal.api.enums.v1.NexusOperationIdConflictPolicy",
+    protoc_gen_elixir_version: "0.16.0",
+    syntax: :proto3
+
+  field :NEXUS_OPERATION_ID_CONFLICT_POLICY_UNSPECIFIED, 0
+  field :NEXUS_OPERATION_ID_CONFLICT_POLICY_FAIL, 1
+  field :NEXUS_OPERATION_ID_CONFLICT_POLICY_USE_EXISTING, 2
+end
--- lib/temporal/api/errordetails/v1/message.pb.ex
+++ lib/temporal/api/errordetails/v1/message.pb.ex
@@ -227,3 +227,15 @@ defmodule Temporal.Api.Errordetails.V1.ActivityExecutionAlreadyStartedFailure do
   field :start_request_id, 1, type: :string, json_name: "startRequestId"
   field :run_id, 2, type: :string, json_name: "runId"
 end
+
+defmodule Temporal.Api.Errordetails.V1.NexusOperationExecutionAlreadyStartedFailure do
+  @moduledoc false
+
+  use Protobuf,
+    full_name: "temporal.api.errordetails.v1.NexusOperationExecutionAlreadyStartedFailure",
+    protoc_gen_elixir_version: "0.16.0",
+    syntax: :proto3
+
+  field :start_request_id, 1, type: :string, json_name: "startRequestId"
+  field :run_id, 2, type: :string, json_name: "runId"
+end
